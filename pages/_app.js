a6e28ec6a31c2745a407b4d985c09bb372aaf146
fajjarnr <zfajart@gmail.com>
2020-12-09 20:54:33 +0700

    adding centext

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
+import { TodosProvider } from "../contexts/TodoContext";
 import "../styles/index.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="container mx-auto py-6 max-w-xl">
-      <Component {...pageProps} />
-    </div>
+    <TodosProvider>
+      <div className="container mx-auto py-6 max-w-xl">
+        <Component {...pageProps} />
+      </div>
+    </TodosProvider>
   );
 }
 
